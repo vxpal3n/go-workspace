5366b5fa31ee5909c9d173045d899a5d60f274ab
Mr. Meow <valenbatam78@gmail.com>
2026-09-09 19:40:55 +0700

    add main & unit tests

--- tugas04/go.mod
+++ tugas04/go.mod
@@ -3,14 +3,18 @@ module tugas04
 go 1.27.0
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.15
+	github.com/jackc/pgx/v5 v5.11.0
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.15 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.11.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -22,5 +26,4 @@ require (
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
---
+++ tugas04/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "context"
+    "log/slog"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
+
+    "tugas04/app/repository"
+    "tugas04/app/service"
+    "tugas04/config"
+    "tugas04/database"
+)
+
+func main() {
+    config.LoadEnv()
+    logger := config.NewLogger()
+
+    pool, err := database.NewPool(context.Background())
+    if err != nil {
+        logger.Error("gagal terhubung ke database", slog.String("error", err.Error()))
+        os.Exit(1)
+    }
+    defer pool.Close()
+
+    studentRepo := repository.NewStudentRepository(pool)
+    studentService := service.NewStudentService(studentRepo)
+
+    app := config.NewApp(logger, pool, studentService)
+
+    port := config.GetEnv("APP_PORT", "3000")
+    go func() {
+        if err := app.Listen(":" + port); err != nil {
+            logger.Error("server berhenti", slog.String("error", err.Error()))
+            os.Exit(1)
+        }
+    }()
+    logger.Info("server berjalan", slog.String("port", port))
+
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    <-quit
+    logger.Info("sinyal berhenti diterima, menutup server")
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+    if err := app.ShutdownWithContext(ctx); err != nil {
+        logger.Error("gagal menutup server dengan rapi", slog.String("error", err.Error()))
+    }
+    logger.Info("server berhenti dengan rapi")
+}
